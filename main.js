8c599b46ce06288cc1593e0ac912f3b467d66742
Braulio Duarte <brauliod617@gmail.com>
2019-09-01 12:40:09 -0400

    add clear button, and redraw grids functionality

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const gridContainer = document.createElement('div');
 const grids = document.createElement("div");
+const clearBtn = document.getElementById('clearBtn');
+
 grids.setAttribute("id", "grid");
 gridContainer.classList.add('center');
 gridContainer.appendChild(grids);
 document.body.appendChild(gridContainer);
 
-function generateGrids(){
-    for(let i = 0; i < 16; i++){
-        for(let y = 0; y < 16; y++){
+function generateGrids(x){
+    for(let i = 0; i < x; i++){
+        for(let y = 0; y < x; y++){
             let grid = document.createElement('div');
             grid.addEventListener('mouseover', pixalate);
             grid.classList.add('grid');
@@ -16,10 +18,42 @@ function generateGrids(){
     }
 }
 
-generateGrids();
+generateGrids(16);
 
 function pixalate(){
-    console.log("BLACK");
     this.style.backgroundColor = "black";
+}
 
+clearBtn.addEventListener('click', clearScreen);
+
+function clearScreen(){
+    let children = grids.childNodes;
+
+     let input = prompt("How many squares per side would you like on new grid?");
+     let regex = /^[0-9]+$/;
+     while(!input.match(regex)){
+        input = prompt("please enter a numerical value");
+     }
+     eraseGrids();
+
+     grids.style.gridTemplateRows = "repeat(" +
+         input + ", " + 480/input + "px [row-start]";
+     grids.style.gridTemplateColumns = "repeat(" +
+         input + ", " + 480/input + "px [col-start]";
+
+     generateGrids(input);
+
+    for(let i = 0; i < grids.childElementCount; i++ ){
+        children[i].style.backgroundColor = "white";
+        children[i].style.width = (480/input).toString() + "px";
+        children[i].style.height = (480/input).toString() + "px";
+    }
+
+}
+
+function eraseGrids(){
+    let count = grids.childElementCount;
+    for(let i = 0; i < count; i++){
+        grids.removeChild(grids.children[0]);
+    }
 }
